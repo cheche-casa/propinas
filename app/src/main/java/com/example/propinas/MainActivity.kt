6f27e9176db9d64555e80f4def287070937f1f8a
cheche-casa <cheche.casa@outlook.es>
2023-05-11 20:00:00 +0200

    Engadir teclado

--- app/src/main/java/com/example/propinas/MainActivity.kt
+++ app/src/main/java/com/example/propinas/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -66,10 +69,13 @@ fun TipTimeScreen() {
 @Composable
 fun EditNumberField() {
     var amountInput by remember { mutableStateOf("") }
-    // o warning de mutableStateOf é un bug que será resolto
     TextField(
+        modifier = Modifier.fillMaxWidth(),
         value = amountInput,
         onValueChange = {amountInput = it}, // it: valor que ten cambiado
+        label = { Text(stringResource(R.string.cost_of_service)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
     )
 }
 
